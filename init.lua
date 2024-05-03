0a67e4055a25e256ba7fdd4a4ec0eb1f761c26da
Alessandro Baccarini <anbaccar@buffalo.edu>
2024-05-02 22:31:41 -0400

    refinining highlight config

--- init.lua
+++ init.lua
@@ -7,9 +7,9 @@ vim.g.maplocalleader = ' '
 
 -- enabling cursor blinking
 vim.opt.guicursor = table.concat({
+  'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100',
   'n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
   'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
-  'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100',
 }, ',')
 
 -- Set to true if you have a Nerd Font installed
@@ -650,20 +650,12 @@ require('lazy').setup({
       local colors = require('dracula').colors()
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=italic'
-      -- vim.cmd.hi('Search gui=underline guibg=' .. colors['selection'] .. ' guifg=' .. colors['fg'])
-      vim.cmd.hi('Search guibg=' .. colors['selection'] .. ' guifg=' .. colors['fg'])
-      -- vim.cmd.hi('LspReferenceWrite gui=underline' .. ' guifg=' .. colors['fg'])
+      vim.cmd.hi('Search guibg=' .. colors['selection'] .. ' guifg=none')
       -- this is used to override the color of the hover highlight from lspconfig
-      vim.cmd.hi('LspReferenceWrite gui=underline' .. ' guifg=none')
-      vim.cmd.hi('LspReferenceRead gui=underline' .. ' guifg=none')
-      vim.cmd.hi('LspReferenceText gui=underline' .. ' guifg=none')
-      -- vim.cmd.hi('LspReferenceText gui=underline' .. ' guifg=' .. colors['fg'])
-      -- vim.cmd.hi 'CursorHold gui=underline'
-      -- vim.cmd.hi 'CursorHoldI gui=underline'
-      vim.cmd.hi('CurSearch guibg=' .. colors['selection'] .. ' guifg=' .. colors['fg'])
-      -- vim.cmd.hi('Cursorline ctermbg=' .. colors['comment'] .. ' guibg=' .. colors['bg'])
-      -- vim.cmd.hi('Cursorline ctermbg=White guibg=' .. colors['bg'])
-      -- vim.cmd.hi('Cursorline term=underline guibg=' .. colors['comment'])
+      vim.cmd.hi('LspReferenceWrite  guifg=none guibg=' .. colors['selection'])
+      vim.cmd.hi('LspReferenceRead   guifg=none guibg=' .. colors['selection'])
+      vim.cmd.hi('LspReferenceText   guifg=none guibg=' .. colors['selection'])
+      vim.cmd.hi('CurSearch gui=underline guibg=' .. colors['selection'] .. ' guifg=none')
     end,
   },
   {
@@ -761,15 +753,6 @@ require('lazy').setup({
     'nvimdev/dashboard-nvim',
     event = 'VimEnter',
     opts = function()
-      --   local logo = [[
-      --      ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-      --      ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
-      --      ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
-      --      ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
-      --      ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
-      --      ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
-      -- ]]
-
       local logo = {
         [[                                                                              ]],
         [[                                                                              ]],
@@ -918,56 +901,3 @@ end
 vim.keymap.set('n', '<C-e>', function()
   toggle_telescope(harpoon:list())
 end, { desc = 'Open harpoon window' })
-
-local dracula = require 'dracula'
-dracula.setup {
-  -- customize dracula color palette
-  colors = {
-    bg = '#282A36',
-    fg = '#F8F8F2',
-    selection = '#44475A',
-    comment = '#6272A4',
-    red = '#FF5555',
-    orange = '#FFB86C',
-    yellow = '#F1FA8C',
-    green = '#50fa7b',
-    purple = '#BD93F9',
-    cyan = '#8BE9FD',
-    pink = '#FF79C6',
-    bright_red = '#FF6E6E',
-    bright_green = '#69FF94',
-    bright_yellow = '#FFFFA5',
-    bright_blue = '#D6ACFF',
-    bright_magenta = '#FF92DF',
-    bright_cyan = '#A4FFFF',
-    bright_white = '#FFFFFF',
-    menu = '#21222C',
-    visual = '#3E4452',
-    gutter_fg = '#4B5263',
-    nontext = '#3B4048',
-    white = '#ABB2BF',
-    black = '#191A21',
-  },
-  -- show the '~' characters after the end of buffers
-  show_end_of_buffer = true, -- default false
-  -- use transparent background
-  transparent_bg = true, -- default false
-  -- set custom lualine background color
-  lualine_bg_color = '#44475a', -- default nil
-  -- set italic comment
-  italic_comment = true, -- default false
-  -- overrides the default highlights with table see `:h synIDattr`
-  overrides = {},
-  -- You can use overrides as table like this
-  -- overrides = {
-  --   NonText = { fg = "white" }, -- set NonText fg to white
-  --   NvimTreeIndentMarker = { link = "NonText" }, -- link to NonText highlight
-  --   Nothing = {} -- clear highlight of Nothing
-  -- },
-  -- Or you can also use it like a function to get color from theme
-  -- overrides = function (colors)
-  --   return {
-  --     NonText = { fg = colors.white }, -- set NonText fg to white of theme
-  --   }
-  -- end,
-}
